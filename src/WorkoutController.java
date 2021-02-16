06ce0ae0f451ea3ca4be6262e0179459df52ae83
Aidan <atl9004@rit.edu>
2021-02-15 19:18:32 -0600

    The skeleton for the MVC is there now

---
+++ src/WorkoutController.java
@@ -0,0 +1,14 @@
+public class WorkoutController {
+
+    /** local instance of the model */
+    private WorkoutModel model;
+
+    /**
+     * construct and set the model
+     * @param model this classes instance of the model
+     */
+    public WorkoutController(WorkoutModel model){
+        this.model = model;
+    }
+
+}
